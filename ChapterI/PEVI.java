44543adc87692a93cba26d56e0eb02f984058bb7
Baumana9301 <148663185+Baumana9301@users.noreply.github.com>
2024-01-09 14:49:18 -0500

    Programming Exercise VI - Summation of a Series

---
+++ ChapterI/PEVI.java
@@ -0,0 +1,11 @@
+//Programming Exercise VI - Summation of a Series
+
+package ChapterI;
+
+public class PEVI
+{
+    public static void main(String[] args)
+    {
+        System.out.println(1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9);
+    }
+}
