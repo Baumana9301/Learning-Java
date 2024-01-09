aed48a7a98d953cc1de8d724cf924c87465d22e7
Baumana9301 <148663185+Baumana9301@users.noreply.github.com>
2024-01-09 14:45:40 -0500

    Programming Exercise V - Compute Expressions

---
+++ ChapterI/PEV.java
@@ -0,0 +1,11 @@
+//Programming Exercise V - Compute Expressions
+
+package ChapterI;
+
+public class PEV
+{
+    public static void main(String[] args)
+    {
+        System.out.println(((9.5 * 4.5) - (2.5 * 3)) / (45.5 - 3.5));
+    }
+}
