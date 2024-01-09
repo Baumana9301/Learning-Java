6089691de0bdfe254f02adaa52332f6aa46abe85
Baumana9301 <148663185+Baumana9301@users.noreply.github.com>
2024-01-09 12:19:00 -0500

    Programming Exercises I & II

---
+++ ChapterI/PEI.java
@@ -0,0 +1,13 @@
+//Programming Exercise 1.1 - Display Three Messages
+
+package ChapterI;
+
+public class PEI
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Welcome to Java");
+        System.out.println("Welcome to Computer Science");
+        System.out.println("Computer Science is fun.");
+    }
+}
---
+++ ChapterI/PEII.java
@@ -0,0 +1,14 @@
+//Programming Exercise 1.2 - Display Five Messages
+package ChapterI;
+
+public class PEII
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Welcome to Java");
+        System.out.println("Welcome to Java");
+        System.out.println("Welcome to Java");
+        System.out.println("Welcome to Java");
+        System.out.println("Welcome to Java");
+    }
+}
