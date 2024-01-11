b5196022f09be66760e1a6070192b7e39537e4c8
Baumana9301 <148663185+Baumana9301@users.noreply.github.com>
2024-01-10 22:32:22 -0500

    Chapter I Programming Exercise VIIII - Area and Perimeter of a Rectangle

---
+++ ChapterI/PEVIIII.java
@@ -0,0 +1,14 @@
+/*Chapter I Programming Exercise VIII
+Area and Perimeter of a Rectangle with
+height of 7.9 and width of 4.5*/
+
+package ChapterI;
+
+public class PEVIIII
+{
+    public static void main(String[] args)
+    {
+        System.out.println(4.5 * 7.9);
+        System.out.println((2 * 4.5) + (2 * 7.9));
+    }
+}
