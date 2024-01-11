5c68ccb6d7270a4218715581a85f989b85d7bfec
Baumana9301 <148663185+Baumana9301@users.noreply.github.com>
2024-01-10 23:04:41 -0500

    Chapter I Programming Exercise XII - Average Speed in km

---
+++ ChapterI/PEXII.java
@@ -0,0 +1,15 @@
+/*Programming Exercise XII
+ Average Speed in Km
+ Assume a runner runs 24 miles in
+ 1 hour, 40 minutes, 35s
+ Convert to km/hr*/
+
+package ChapterI;
+
+public class PEXII
+{
+    public static void main(String[] args)
+    {
+        System.out.println((24 * 1.6 * 60) / (100 + (7.0 / 2)));
+    }
+}
