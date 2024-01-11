d94558acba125910acf51ea221275476e3f79970
Baumana9301 <148663185+Baumana9301@users.noreply.github.com>
2024-01-10 22:28:09 -0500

    Chapter I Programming Exercise VIII - Area and Perimeter of a Circle

---
+++ ChapterI/PEVIII.java
@@ -0,0 +1,14 @@
+/*Chapter I Practice Exercise VIII 
+Area and Perimeter of a Circle given
+radius of 5.5*/
+
+package ChapterI;
+
+public class PEVIII
+{
+    public static void main(String[] args)
+    {
+        System.out.println(2 * 3.14159 * 5.5);
+        System.out.println(5.5 * 5.5 * 3.14159);
+    }
+}
