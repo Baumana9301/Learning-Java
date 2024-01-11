48a928b33bb17ea7e18d29cbcf353d1dc1bb5fa3
Baumana9301 <148663185+Baumana9301@users.noreply.github.com>
2024-01-10 22:39:21 -0500

    Chapter I Programming Exercise X - Average Speed in mph

---
+++ ChapterI/PEX.java
@@ -0,0 +1,12 @@
+/*Chapter I Practice Program X 
+Average Speed in Miles*/
+
+package ChapterI;
+
+public class PEX
+{
+    public static void main(String[] args)
+    {
+        System.out.println((14 * 60) / (45.5 * 1.6));
+    }
+}
