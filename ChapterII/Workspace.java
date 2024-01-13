513e5a98b69fa9ad903498a850111dbfbf26ec33
Baumana9301 <148663185+Baumana9301@users.noreply.github.com>
2024-01-12 21:03:59 -0500

    Update Workspace.java

--- ChapterII/Workspace.java
+++ ChapterII/Workspace.java
@@ -1,23 +1,15 @@
-//01.11.24 Workspace File 
-package ChapterII;
+//01.12.24 Workspace File 
 
-import java.util.Scanner;
+package ChapterII;
 
 public class Workspace
 {
     public static void main(String[] args)
     {
-        Scanner input = new Scanner(System.in);
-        double n1, n2, n3, average;
+        int i, j, k;
 
-        System.out.print("Please enter 3 numbers: ");
-        n1 = input.nextDouble();
-        n2 = input.nextDouble();
-        n3 = input.nextDouble();
+        i = j = k = 2;
 
-        average = (n1 + n2 + n3) / 3;
-
-        System.out.println("The average of " + n1 + ", " +
-            n2 + ", and " + n3 + " is " + average);
+        System.out.println(i + " " + j + " " + k);
     }
 }
