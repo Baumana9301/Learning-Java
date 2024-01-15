7bfb605b3b751df78016785b5490080a4efe4736
Baumana9301 <148663185+Baumana9301@users.noreply.github.com>
2024-01-14 22:26:21 -0500

    Chapter II - 2.9 - CheckPoint 2.9.7,8

---
+++ ChapterII/CheckPoint2_9_7.java
@@ -0,0 +1,17 @@
+/*Chapter II CheckPoint 2.9.7,8
+ Math.pow(a, b) practice */
+
+package ChapterII;
+
+public class CheckPoint2_9_7 {
+    public static void main(String[] args)
+    {   
+        int m, r;
+        double n1 = Math.pow(2,3.5);
+
+        m = 2;
+        r = 2;
+        System.out.println(n1);
+        System.out.println(m * (Math.pow(r, 2)));
+    }
+}
